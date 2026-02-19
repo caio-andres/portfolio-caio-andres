a9628f0a9705e7eb66c12ed0575d9c2eb6ac978e
Caio Andres <caioandresgit@outlook.com>
2026-02-19 16:30:55 -0300

    style(background): Using Magic UI to make the background's style.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Hero from "@/components/Hero";
 import AboutContent from "@/components/AboutContent";
 import Experience from "@/components/Experience";
@@ -5,11 +6,14 @@ import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import SectionLayout from "@/components/SectionLayout";
 import Footer from "@/components/Footer";
+import { StripedPattern } from "@/components/magicui/striped-pattern";
 
 export default function Home() {
   return (
-    <main className="relative">
-      <div className="pt-8">
+    <main className="relative overflow-hidden">
+      <StripedPattern className="fixed inset-0 z-0 opacity-[0.03] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]" />
+
+      <div className="relative z-10 pt-8">
         <div className="container mx-auto max-w-5xl px-6">
           <Hero />
           <SectionLayout
